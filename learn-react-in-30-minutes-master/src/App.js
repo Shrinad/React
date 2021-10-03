5b2f07d4a57bf5cb069dcd1e34104ea2a4eee242
INAMDAR Shrinad <F3Y@dsone.3ds.com>
2021-10-03 13:51:32 -0400

    Small change

--- learn-react-in-30-minutes-master/src/App.js
+++ learn-react-in-30-minutes-master/src/App.js
@@ -42,7 +42,7 @@ function App() {
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleAddTodo}>Add new Todo</button>
       <button onClick={handleClearTodos}>Clear Complete</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
